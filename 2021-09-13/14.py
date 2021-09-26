4a21f5ac3d1c5db8ca0a435b6cffa32d55242a12
andrey.pirogov <prgv@pm.me>
2021-09-27 02:02:39 +0300

    some errors fixed

--- 2021-09-13/14.py
+++ 2021-09-13/14.py
@@ -7,12 +7,13 @@ turtle.shape('turtle')
 turtle.speed('fastest')
 
 
-def mnogoug(n, l, alpha, decimal, angle):
+def mnogoug(n, l, alpha, decimal):
+    angle = 180 - 180/n
     turtle.setheading(alpha)
     for i in range(1, math.floor(n * decimal) + 1, 1):
         turtle.forward(l)
         turtle.left(angle)
 
-# mnogoug(5, 150, 0, 1, 144)
-mnogoug(11, 150, 0, 1, 163.64)
+mnogoug(5, 150, 0, 1)
+# mnogoug(11, 150, 0, 1)
 turtle.done()
--- 2021-09-13/9.py
+++ 2021-09-13/9.py
@@ -15,9 +15,11 @@ def mnogoug(n, l, alpha):
 
 
 for i in range(3, 10):
-    R = i * 10 / np.sin(np.pi / i)
+    R = i*20/np.sin(3.14/i)*0.75
     turtle.penup()
-    turtle.setx(R)
-    turtle.sety(0)
+    # turtle.setx(-R*np.cos(6.28/i))
+    # turtle.sety(-R*np.sin(6.28/i))
+    turtle.setx(R**np.cos(3.14/2 * (1- 1/i))+10*i)
+    turtle.sety(R**np.sin(3.14/2 * (1- 1/i))-10*i)
     turtle.pendown()
-    mnogoug(i, i * 20)
+    mnogoug(i, i * 20, 180)
