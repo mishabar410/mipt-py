135d7068bba913157c5805530d8018c4412fdef1
andrey.pirogov <prgv@pm.me>
2021-09-27 02:44:59 +0300

    new fix

--- 2021-09-13/7.py
+++ 2021-09-13/7.py
@@ -6,7 +6,7 @@ turtle.speed('fastest')
 
 
 def spiral(a, dphi):
-    for i in range(1, 1000, 1):
+    for i in range(1, 200, 1):
         turtle.forward(a * dphi * (1 + (i * dphi)**2)**0.5)
         turtle.left(dphi)
 
